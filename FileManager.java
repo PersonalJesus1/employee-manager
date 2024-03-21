2a26b6891a2df77dc362f1e6fa65a11b7e9e5e17
Bizunova_Yuliya <93055122+PersonalJesus1@users.noreply.github.com>
2024-03-21 04:15:14 +0000

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -1,4 +1,18 @@
 package Lessons.Employee_manager;
 
+import java.util.ArrayList;
+
 public class FileManager {
+    public static  ArrayList<Employee> employees = new ArrayList<>();
+
+    public static void addEmployee( Employee employee){
+        employees.add(employee);
+    }
+
+    public static void removeEmployee( Employee employee){
+        employees.remove(employee);
+
+    }
+
+
 }

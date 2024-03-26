4a3a8b9fb8aa7b70301699ca37ddab477b4608a8
Bizunova_Yuliya <93055122+PersonalJesus1@users.noreply.github.com>
2024-03-26 03:19:20 +0000

    Update HeadOfHr.java

--- positions/HeadOfHr.java
+++ positions/HeadOfHr.java
@@ -1,32 +1,33 @@
-package Lessons.Employee_manager;
+package Lessons.Employee_manager.positions;
+
+import Lessons.Employee_manager.departments.Department;
 
 import java.math.BigDecimal;
 
-public class HeadOfHr implements Position {
-
+public class JuniorDeveloper implements Position {
 
     @Override
     public String getName() {
-        return "Head Of HR";
+        return "JuniorDeveloper";
     }
 
     @Override
     public Department getDepartment() {
-        return Department.HR;
+        return Department.DEVELOPMENT;
     }
 
     @Override
-    public BigDecimal getMinSalary() {
-        return new BigDecimal(800);
+    public  BigDecimal getMinSalary() {
+        return new BigDecimal(400);
     }
 
     @Override
     public BigDecimal getMaxSalary() {
-        return new BigDecimal(1700);
+        return new BigDecimal(1000);
     }
+
     @Override
     public boolean validateSalary(int newSalary) {
-        return newSalary>= 800 && newSalary <= 1700;
+        return newSalary>= 400 && newSalary <= 1000;
     }
 }
-
